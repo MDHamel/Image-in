d7f6fd2764df65f29b356260aa3aebad481b7f8b
Matt H <matthamel321@gmail.com>
2022-09-12 14:51:19 -0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import './gallery.css';
+import './navbar.css'
+import content from "./content.json";
 
-function App() {
+import React, { useState } from 'react';
+
+
+
+export default function App() {
+  const [display, setDisplay] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Navbar />
+
+      <div className={'overlay '.concat(display?"reveal":"hidden")} >
+        <aside className='offclick' onClick={()=>{setDisplay("")}} />
+        <img className='overlayImg' src={`${display.locale}`}  alt={display.alt}/>
+        
+        <section className='right'>
+          <p className='close' onClick={()=>{setDisplay("")}}>X</p>
+          <p className='overlayTitle'>{display.title}</p>
+          <p className='desc'>{display.desc}</p>
+        </section>
+      </div>
+
+      <div className="gallery">
+        {content.images.map((image, i) => (
+          <Card data={image} key={i} onClick={()=>{setDisplay(image)}} />
+        ))}
+      </div>
+    </React.Fragment>
   );
 }
 
-export default App;
+
+function Card(props){
+
+  return(
+    <div className={"card"}  onClick={props.onClick} >
+      <p className='title'>{props.data.title}</p>
+      <img className='image' src={`${props.data.locale}`} alt={props.alt}/>
+    </div>
+    
+  )
+}
+
+function Navbar(){
+  return(
+    <nav>
+      <h1 className='logo'>IMAGE - <span>IN</span></h1>
+
+      <p className='upload'>Add Image</p>
+    </nav>
+  )
+}
