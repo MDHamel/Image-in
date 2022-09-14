e7f7397ad728d7b2a15bc9322b87affeef7f44fc
Matt H <matthamel321@gmail.com>
2022-09-14 16:54:52 -0700

    small update
    added upload functionality, still in dev

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './gallery.css';
-import './navbar.css'
+import './navbar.css';
+import './overlay.css';
+import './uploader.css';
 import content from "./content.json";
 
 import React, { useState } from 'react';
@@ -7,27 +9,23 @@ import React, { useState } from 'react';
 
 
 export default function App() {
-  const [display, setDisplay] = useState("");
+  const [overlay, setOverlay] = useState("");
+  const [uploadState, setUploadeState] = useState("hidden")
+
   return (
     <React.Fragment>
-      <Navbar />
 
-      <div className={'overlay '.concat(display?"reveal":"hidden")} >
-        <aside className='offclick' onClick={()=>{setDisplay("")}} />
-        <img className='overlayImg' src={`${display.locale}`}  alt={display.alt}/>
-        
-        <section className='right'>
-          <p className='close' onClick={()=>{setDisplay("")}}>X</p>
-          <p className='overlayTitle'>{display.title}</p>
-          <p className='desc'>{display.desc}</p>
-        </section>
-      </div>
+      <Navbar uploder={()=>{setUploadeState("reveal");console.log(uploadState)}}/>
+
+      <Overlay display={overlay} closer={()=>{setOverlay("");}} />
+      <Uploader closer={()=>setUploadeState("hidden")} display={uploadState}/>
 
       <div className="gallery">
         {content.images.map((image, i) => (
-          <Card data={image} key={i} onClick={()=>{setDisplay(image)}} />
+          <Card data={image} key={i} onClick={()=>{setOverlay(image)}} />
         ))}
       </div>
+
     </React.Fragment>
   );
 }
@@ -40,16 +38,38 @@ function Card(props){
       <p className='title'>{props.data.title}</p>
       <img className='image' src={`${props.data.locale}`} alt={props.alt}/>
     </div>
-    
   )
 }
 
-function Navbar(){
+function Navbar(props){
   return(
     <nav>
       <h1 className='logo'>IMAGE - <span>IN</span></h1>
-
-      <p className='upload'>Add Image</p>
+      <p className='upload' onClick={props.uploder}>Add Image</p>
     </nav>
+  )
+}
+
+function Uploader(props){
+  return(
+    <div className={'uploader '.concat(props.display)}>
+      <section className='offclick' onClick={(props.closer)} />
+      
+    </div>
+  )
+}
+
+function Overlay(props){
+  return(
+    <div className={'overlay '.concat(props.display?"reveal":"hidden")} >
+        <section className='offclick' onClick={props.closer} />
+        <img className='overlayImg' src={`${props.display.locale}`}  alt={props.display.alt}/>
+        
+        <aside className='right'>
+          <p className='close' onClick={props.closer}>X</p>
+          <p className='overlayTitle'>{props.display.title}</p>
+          <p className='desc'>{props.display.desc}</p>
+        </aside>
+    </div>
   )
 }
