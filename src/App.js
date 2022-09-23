7c92bb067b1cb79633caae1837f0585d459b4dc4
Matt H <matthamel321@gmail.com>
2022-09-23 14:25:00 -0700

    Uploader almost done
    Uploader almost working, need to find a cloud service to upload images to or something of the sort.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './overlay.css';
 import './uploader.css';
 import content from "./content.json";
 
-import React, { useState } from 'react';
+import React, { useRef,useState } from 'react';
+
 
 
 
@@ -44,16 +45,87 @@ function Card(props){
 function Navbar(props){
   return(
     <nav>
-      <h1 className='logo'>IMAGE - <span>IN</span></h1>
+      <h1 className='logo'>IMAGE <span>IN</span></h1>
       <p className='upload' onClick={props.uploder}>Add Image</p>
     </nav>
   )
 }
 
 function Uploader(props){
+  const [image, setImage] = useState("");
+  const inputFile = useRef(null);
+  const src = image ==="" ? "../assets/images/imageplaceholder.png" : URL.createObjectURL(image);
+  const acceptedFileTypes = ".jpg, .jpeg, .gif, .png, .webp";
+  const [uploadError, setError] = useState("");
+
+  const closeUploader = () => {
+    props.closer(); 
+    setImage(""); 
+    document.getElementById("image-uploader").reset();
+  }
+
+  const onUploadTextClick = () => {
+      inputFile.current.click();
+  };
+
+  const uploadFileHandler = (e) => {
+    const { files } = e.target;
+    if (files && files.length) {
+
+      setImage(files[0]);
+      
+    }
+  };
+
+ const submitUpload = async (e) => {
+    e.preventDefault();
+    if(!(image && e.target.title.value && e.target.desc.value)){
+      setError("error");
+    }
+    else{
+
+      //need a cloud storage or somewhere for the images to be uploaded
+
+            
+      // content.append(
+      //   {
+      //       "id": content.length,
+      //       "title": e.target.title.value,
+      //       "desc": e.target.desc.value,
+      //       "locale": "../assets/images/The_Puppy.jpg",
+      //       "alt":e.target.alt.value
+      //   }
+      // );
+
+      //closeUploader();
+    }
+
+  }
+
   return(
-    <div className={'uploader '.concat(props.display)}>
-      <section className='offclick' onClick={(props.closer)} />
+    <div className={'uploader '.concat(props.display)} >
+      <section className='offclick' onClick={closeUploader}/>
+
+      <form id="image-uploader" onSubmit={submitUpload}>
+      
+          <aside className='uploaderLeft' onClick={onUploadTextClick}>
+              <p>Click Here to Upload</p>
+            
+            <img  id={image ==="" ? "imageplaceholder" : ""} src= {src} alt="Uploaded" />
+            <input id="fileIn" name="file" type="file" ref={inputFile} onChange={uploadFileHandler} accept={acceptedFileTypes}/>
+          </aside>
+        
+      
+        <aside className='uploaderRight'>
+          <section className='formOffset'>
+            <input id="title" name="title" type="text" placeholder="Title" />
+            <textarea id="desc" name="desc" placeholder="Enter your description here!" />
+            <input id="alt" name="alt" type="text" placeholder="Alternate Text (Optional)"  />
+            
+            <button className={uploadError} type="submit" onAnimationEnd={()=>{setError("")}}> Upload </button>
+          </section>
+        </aside>
+      </form>
       
     </div>
   )
@@ -65,7 +137,7 @@ function Overlay(props){
         <section className='offclick' onClick={props.closer} />
         <img className='overlayImg' src={`${props.display.locale}`}  alt={props.display.alt}/>
         
-        <aside className='right'>
+        <aside className='rightOverlay'>
           <p className='close' onClick={props.closer}>X</p>
           <p className='overlayTitle'>{props.display.title}</p>
           <p className='desc'>{props.display.desc}</p>
